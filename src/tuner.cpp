6c81f8d1ffbb043e9bd3c88852e6834ea15f8998
Jon Dart <arasan-dev@arasanchess.org>
2015-03-28 07:45:56 -0700

    remove other optimizer code

--- src/tuner.cpp
+++ src/tuner.cpp
@@ -9,6 +9,7 @@
 #include "util.h"
 #include "search.h"
 #include "tune.h"
+#include "spsa.h"
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -19,16 +20,7 @@ extern "C" {
 #include <unistd.h>
 };
 
-#define ROCKSTAR
-
-#ifdef ROCKSTAR
-#include "Rockstar.hpp"
-#include <Eigen/Core>
-static int min_cost = 5000;
-#else
-#include "cmaes.h"
-using namespace libcmaes;
-#endif
+static int iterations = 40;
 
 // script to run matches
 static const char * MATCH_PATH="/home/jdart/tools/match.py";
@@ -133,73 +125,22 @@ static double computeLsqError() {
    return double(5000-exec(cmd.c_str()));
 }
 
-#ifdef ROCKSTAR
-static double evaluate(const Eigen::VectorXd &x, const int dim) 
+static double evaluate(const vector<double> &x)
 {
-   int i;
-   for (i = 0; i < tune::NUM_TUNING_PARAMS; i++) 
+   for (int i = 0; i < tune::NUM_TUNING_PARAMS; i++) 
    {
-      tune::tune_params[i].current = unscale(logUnXForm(x(i)),tune::tune_params[i]);
+      tune::tune_params[i].current = round(unscale(logUnXForm(x[i]),tune::tune_params[i]));
    }
    tune::initParams();
    double err = computeLsqError();
    return err;
 };
 
-static void writeProgress(Eigen::VectorXd &theta)
+void update(double obj, const vector<double> &x) 
 {
-   if (x0_file_name.length()) {
-      ofstream x0_out(x0_file_name,ios::out | ios::trunc);
-      tune::writeX0(x0_out);
-   } else {
-      tune::writeX0(cout);
-   }
-   if (out_file_name.length()) {
-      ofstream param_out(out_file_name,ios::out | ios::trunc);
-      Scoring::Params::write(param_out);
-      param_out << endl;
-   } else {
-      Scoring::Params::write(cout);
-      cout << endl;
-   }
+   cout << "objective=" << obj << endl;
 };
 
-#else
-
-static FitFunc evaluator = [](const double *x, const int dim) 
-{
-   int i;
-   for (i = first_index; i <= last_index; i++) 
-   {
-      tune::tune_params[i].current = unscale(x[i-first_index],tune::tune_params[i]);
-   }
-   tune::initParams();
-   double err = computeLsqError();
-   cout << "objective=" << err << endl;
-   return err;
-   
-};
-
-static ProgressFunc<CMAParameters<GenoPheno<pwqBoundStrategy>>,CMASolutions> progress = [](const CMAParameters<GenoPheno<pwqBoundStrategy>> &cmaparams, const CMASolutions &cmasols)
-{
-   
-   std::cout << "best solution: " << cmasols << std::endl;
-   vector <double> x0 = cmasols.best_candidate().get_x();
-/*
-   cout << "denormalized solution: " << endl;
-   int i = 0;
-        
-   for (vector<double>::const_iterator it = x0.begin();
-        it != x0.end();
-        it++,i++) {
-      cout << " " << tune_params[i].name << ": " << round(unscale(*it,i)) << endl;
-   }
-   cout << endl;
-*/   
-   return 0;
-};
-
-#endif
 
 static void usage() 
 {
@@ -300,46 +241,12 @@ int CDECL main(int argc, char **argv)
     cout << "dimension = " << dim << endl;
     cout << "games per core per iteration = " << games << endl;
     
-#ifdef ROCKSTAR
-    Eigen::VectorXd x0 = Eigen::VectorXd::Zero(tune::NUM_TUNING_PARAMS,1);
-    // scale and transform initial solution
-    for (int i = 0; i < tune::NUM_TUNING_PARAMS; i++) {
-       x0(i) = logXForm(scale(tune::tune_params[i]));
-    }
-    Eigen::VectorXd Initial_StandardDeviation = Eigen::VectorXd::Ones(tune::NUM_TUNING_PARAMS,1) * 0.05;
-    Eigen::VectorXd theta = Eigen::VectorXd::Zero(tune::NUM_TUNING_PARAMS,1);
-
-    int initial_exp = 2;
-    rockstar::Rockstar optimizer(x0,Initial_StandardDeviation, initial_exp);
-
-    for(int i=0; i<1200; i++){
-      optimizer.getNextTheta2Evaluate(theta);
-      double cost = evaluate(theta,tune::NUM_TUNING_PARAMS);
-      optimizer.setTheCostFromTheLastTheta(cost);
-
-      std::cout<<optimizer.getRolloutNumber()<<"th rollout, this cost is   "<<cost<<", sigma is "<<optimizer.getSigma()<<std::endl;
-      if (cost < min_cost) {
-         writeProgress(theta);
-         min_cost = cost;
-      }
-    }
-#else
+    Spsa s(tune::NUM_TUNING_PARAMS);
     vector<double> x0;
-    double sigma = 0.05;
-    // use variant with box bounds
-    double lbounds[tune::NUM_TUNING_PARAMS],
-       ubounds[tune::NUM_TUNING_PARAMS];
-    // initialize & normalize
-    for (int i = first_index; i <= last_index; i++) {
-       x0.push_back(scale(tune::tune_params[i]));
-       lbounds[i-first_index] = 0.0;
-       ubounds[i-first_index] = 1.0;
+    for (int i = 0; i < tune::NUM_TUNING_PARAMS; i++) {
+       x0.push_back(logXForm(scale(tune::tune_params[i])));
     }
-    GenoPheno<pwqBoundStrategy> gp(lbounds,ubounds,dim);
-    CMAParameters<GenoPheno<pwqBoundStrategy>> cmaparams(dim,&x0.front(),sigma,-1,0,gp);
-    cmaparams.set_algo(sepaBIPOP_CMAES);
-    CMASolutions cmasols = cmaes<GenoPheno<pwqBoundStrategy>>(evaluator,cmaparams,progress);
-#endif
+    s.optimize(x0,iterations,evaluate,update);
 
     return 0;
 }
