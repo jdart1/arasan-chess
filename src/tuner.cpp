26ada28ed8f54d70dc4430240705f9f86de28a5a
Jon Dart <arasan-dev@arasanchess.org>
2015-04-03 18:27:17 -0700

    add support for CMA-ES, too

--- src/tuner.cpp
+++ src/tuner.cpp
@@ -20,8 +20,13 @@ extern "C" {
 #include <unistd.h>
 };
 
+//#define CMAES
+
+#include "cmaes.h"
+using namespace libcmaes;
+
 // define to run optimizer code on a simple test problem
-//#define TEST
+#define TEST
 
 static int iterations = 40;
 
@@ -113,6 +118,69 @@ static double computeLsqError() {
    // Optimizer minimizes, so optimize 5000-rating.
    return double(5000-exec(cmd.c_str()));
 }
+
+#ifdef CMAES
+static FitFunc evaluator = [](const double *x, const int dim) 
+{
+   int i;
+   for (i = 0; i < dim; i++) 
+   {
+      tune::tune_params[i+first_index].current = unscale(x[i+first_index],tune::tune_params[i+first_index]);
+   }
+   tune::initParams();
+   double err = computeLsqError();
+   cout << "objective=" << err << endl;
+   return err;
+   
+};
+
+static FitFunc test_evaluator = [](const double *x, const int dim) 
+{
+   double sum = 0;
+   for (int i = 0; i < dim; i++) {
+      double factor = 0.8;
+      if (i % 2 == 0) factor = 1.1;
+      sum += factor*pow(x[i],2.0);
+   }
+   cout << "returning " << 800.0*sqrt(sum) << endl;
+   return 800.0*sqrt(sum);
+};
+
+static ProgressFunc<CMAParameters<GenoPheno<pwqBoundStrategy>>,CMASolutions> progress = [](const CMAParameters<GenoPheno<pwqBoundStrategy>> &cmaparams, const CMASolutions &cmasols)
+{
+   
+   std::cout << "best solution: " << cmasols << std::endl;
+   vector <double> x0 = cmasols.best_candidate().get_x();
+/*
+   cout << "denormalized solution: " << endl;
+   int i = 0;
+        
+   for (vector<double>::const_iterator it = x0.begin();
+        it != x0.end();
+        it++,i++) {
+      cout << " " << tune_params[i].name << ": " << round(unscale(*it,i)) << endl;
+   }
+   cout << endl;
+*/   
+#ifndef TEST
+   if (x0_file_name.length()) {
+      ofstream x0_out(x0_file_name,ios::out | ios::trunc);
+      tune::writeX0(x0_out);
+   } else {
+      tune::writeX0(cout);
+   }
+   if (out_file_name.length()) {
+      ofstream param_out(out_file_name,ios::out | ios::trunc);
+      Scoring::Params::write(param_out);
+      param_out << endl;
+   } else {
+      Scoring::Params::write(cout);
+      cout << endl;
+   }
+#endif
+   return 0;
+};
+#endif
 
 #ifdef TEST
 static double evaluate(const vector<double> &x) 
@@ -249,21 +317,51 @@ int CDECL main(int argc, char **argv)
     cout << "dimension = " << dim << endl;
     cout << "games per core per iteration = " << games << endl;
     
+    vector<double> x0;
+#ifdef CMAES
+    double sigma = 0.05;
+    // use variant with box bounds
+    double lbounds[dim],
+       ubounds[dim];
+#ifdef TEST
+    dim = 10;
+    for (int i = 0; i < dim; i++) {
+       x0.push_back(0.8);
+       lbounds[i] = 0.0;
+       ubounds[i] = 1.0;
+    }
+    GenoPheno<pwqBoundStrategy> gp(lbounds,ubounds,dim);
+    CMAParameters<GenoPheno<pwqBoundStrategy>> cmaparams(dim,&x0.front(),sigma,-1,0,gp);
+    cmaparams.set_algo(sepaBIPOP_CMAES);
+    cmaparams.set_max_fevals(iterations);
+    CMASolutions cmasols = cmaes<GenoPheno<pwqBoundStrategy>>(test_evaluator,cmaparams,progress);
+#else
+    // initialize & normalize
+    for (int i = 0; i < dim; i++) {
+       x0.push_back(scale(tune::tune_params[i+first_index]));
+       lbounds[i] = 0.0;
+       ubounds[i] = 1.0;
+    }
+    GenoPheno<pwqBoundStrategy> gp(lbounds,ubounds,dim);
+    CMAParameters<GenoPheno<pwqBoundStrategy>> cmaparams(dim,&x0.front(),sigma,-1,0,gp);
+    cmaparams.set_algo(sepaBIPOP_CMAES);
+    cmaparams.set_max_iter(iterations);
+    CMASolutions cmasols = cmaes<GenoPheno<pwqBoundStrategy>>(evaluator,cmaparams,progress);
+#endif
+#else
 #ifdef TEST
     Spsa s(10);
-    vector <double> x0;
     for (int i = 0; i < 10; i++) {
        x0.push_back(0.8);
     }
     s.optimize(x0,iterations,evaluate,update);
 #else
     Spsa s(tune::NUM_TUNING_PARAMS);
-    vector<double> x0;
     for (int i = 0; i < tune::NUM_TUNING_PARAMS; i++) {
        x0.push_back(scale(tune::tune_params[i]));
     }
     s.optimize(x0,iterations,evaluate,update);
 #endif
-
+#endif
     return 0;
 }
