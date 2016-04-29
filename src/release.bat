6c497ee10c57670dc61d0309c0b5ecf4de4ea2b1
Jon Dart <arasan-dev@arasanchess.org>
2016-04-29 10:47:59 -0700

    add license files to release.bat

--- src/release.bat
+++ src/release.bat
@@ -9,6 +9,7 @@ pushd ..
 7z u -tzip -orelease %1 prj\prj.sln prj\prj.vcxproj
 7z u -tzip -orelease %1 book\basic.pgn book\eco doc\changes.txt 
 7z u -tzip -orelease %1 doc\programr.html doc\style.css
+7z u -tzip -orelease %1 doc\*license*.txt
 7z u -tzip -orelease %1 tests\bt2630.epd tests\ecmgcp.epd
 7z u -tzip -orelease %1 tests\arasan18.epd tests\pet.epd tests\prof
 7z u -tzip -orelease %1 tests\eet.epd tests\tests
