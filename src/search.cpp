9e8f6144099fa74937360c80bb23686ff675a760
Jon Dart <arasan-dev@arasanchess.org>
2017-02-07 00:08:46 -0800

    Revert back to 19.2 values for LMP parameters - seems to be a bit better at blitz time controls.

--- src/search.cpp
+++ src/search.cpp
@@ -81,9 +81,9 @@ static int singularExtensionDepth(int depth)
 
 static int CACHE_ALIGN LMR_REDUCTION[2][64][64];
 
-static const int LMP_DEPTH=6;
+static const int LMP_DEPTH=10;
 
-static const int LMP_MOVE_COUNT[7] = {3, 6, 10, 16, 24, 33, 44};
+static const int LMP_MOVE_COUNT[11] = {3, 3, 5, 9, 15, 23, 33, 45, 59, 75, 93};
 
 static const int RAZOR_MARGIN[4] = { int(0.9*PAWN_VALUE),
                                      int(1.25*PAWN_VALUE),
