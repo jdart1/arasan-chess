a9b90e4e2cc14cef40f04fd281cdd8e506ee3eed
Jon Dart <arasan-dev@arasanchess.org>
2018-01-03 09:40:48 -0800

    update version

--- src/constant.h
+++ src/constant.h
@@ -1,4 +1,4 @@
-// Copyright 1994-2017 by Jon Dart.  All Rights Reserved.
+// Copyright 1994-2018 by Jon Dart.  All Rights Reserved.
 
 #ifndef _CONSTANT_H
 #define _CONSTANT_H
@@ -25,6 +25,6 @@ const int INFINITE_TIME = 9999999;
 #define MAKE_STR(x) STRINGIFY(x)
 #define DEPTH_INCREMENT 4
 #define Arasan_Version MAKE_STR(ARASAN_VERSION)
-#define Arasan_Copyright "Copyright 1994-2017 by Jon Dart. All Rights Reserved."
+#define Arasan_Copyright "Copyright 1994-2018 by Jon Dart. All Rights Reserved."
 
 #endif
