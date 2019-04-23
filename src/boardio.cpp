3e155fecebf02b91f20cb05a1058f50e495e646d
dsjoerg <dsjoerg@gmail.com>
2019-04-23 13:40:09 -0400

    better handle boards without kings

--- src/boardio.cpp
+++ src/boardio.cpp
@@ -158,6 +158,18 @@ int BoardIO::readFEN(Board &board, const string &buf)
    ASSERT(board.state.moveCount+1<Board::RepListSize);
    board.repList[board.state.moveCount++] = board.hashCode();
 
+   // if board has no kings, clear king-related structures
+   if (board.contents[board.kingPos[White]]!=WhiteKing) {
+     board.kingPos[White] = InvalidSquare;
+     board.state.castleStatus[White] = CantCastleEitherSide;
+     board.material[White].removePiece(King);
+   }
+   if (board.contents[board.kingPos[Black]]!=BlackKing) {
+     board.kingPos[Black] = InvalidSquare;
+     board.state.castleStatus[Black] = CantCastleEitherSide;
+     board.material[Black].removePiece(King);
+   }
+   
    return 1;
 }
 
