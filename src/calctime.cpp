788394522de8805898bd8a8ba68bf514bf902290
Jon Dart <chess1@jondart.name>
2019-10-21 04:29:29 -0700

    Further changes to time management LLR=3.15 [-2.94,2.94] (4328 games) result = H1

--- src/calctime.cpp
+++ src/calctime.cpp
@@ -6,6 +6,9 @@
 
 static const int DEFAULT_MOVES_TO_TC = 28;
 static const float PONDER_FACTOR = 1.4F;
+static const int EXTRA_TIME_FACTOR_INC = 8;
+static const int EXTRA_TIME_FACTOR_NO_INC = 12;
+static const double EXTRA_TIME_MULT = 2.5;
 
 int timeMgmt::calcTimeLimit(int moves, int incr, int time_left, bool ponder, int trace)
 {
@@ -26,8 +29,7 @@ int timeMgmt::calcTimeLimitUCI(int movestogo, int incr,
 {
     if (trace) cout << "# movestogo=" << movestogo << " time_left=" << time_left << endl;
     if (movestogo == 0) movestogo = DEFAULT_MOVES_TO_TC;
-    time_left = std::max<int>(0, time_left - options.search.move_overhead);
-    int time_target = (std::max<int>(0,movestogo-1)*incr + time_left)/movestogo;
+    int time_target = std::max(0,(std::max<int>(0,movestogo-1)*incr + time_left)/movestogo - options.search.move_overhead);
 
     if (ponder && movestogo > 4) {
         time_target = (int)(PONDER_FACTOR*time_target);
@@ -41,11 +43,11 @@ int timeMgmt::calcTimeLimitUCI(int movestogo, int incr,
 }
 
 int timeMgmt::calcExtraTime(int time_left, int time_target, int inc) {
-    time_left = std::max(0,time_left - options.search.move_overhead);
-    if ((inc == 0 && time_left > time_target*10) ||
-      (inc > 0 && time_left > time_target*6))
-      return int(time_target*2.5);
-    else
-      return 0;
-    //return std::min<int>(time_left/(inc > 0 ? 15 : 20),time_target*2.5);
+    if (inc == 0 && time_left < time_target*EXTRA_TIME_FACTOR_NO_INC) {
+        return std::max<int>(0,int(-EXTRA_TIME_MULT*time_target + 2*(EXTRA_TIME_MULT/EXTRA_TIME_FACTOR_NO_INC)*time_left));
+    } else if (inc > 0 && time_left < time_target*EXTRA_TIME_FACTOR_INC) {
+        return std::max<int>(0,int(-EXTRA_TIME_MULT*time_target + 2*(EXTRA_TIME_MULT/EXTRA_TIME_FACTOR_INC)*time_left));
+    } else {
+        return int(EXTRA_TIME_MULT*time_target);
+    }
 }
