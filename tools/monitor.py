1f87bb641ae3da9ea414cf39f4d3b3f0e107ad99
Jon Dart <chess1@jondart.name>
2020-03-20 15:30:06 -0700

    add stats to tool directory as submodule
    update monitor to use Fishtest stats code

--- tools/monitor.py
+++ tools/monitor.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys, subprocess, math, time, threading
+# Copyright 2019, 2020 by Jon Dart. All Rights Reserved.
+#
+import json, sys, subprocess, math, time, threading
 from subprocess import Popen, PIPE, call
+# copy of Fishtest stats module
+# copy because it is a nested package in Fishtest
+from stats import stat_util
 
 # Script to monitor cutechess logs on multiple machines and
 # compute SPRT.
@@ -21,79 +26,13 @@ REMOTE_EXEC_SCRIPT = 'python3 /home/jdart/tools/match_status.py'
 # script to stop matches
 STOP_SCRIPT = '/home/jdart/tools/stop-all'
 
-# list of hosts, adjust as needed
-hosts = ['sassy','puppy','wombat','localhost']
+POLL_INTERVAL = 180
 
 scores = [0,0,0]
 
-limit = 0
+ELO_MODEL='logistic'
 
 class Monitor:
-
-# following code is from Michel Van Den Bergh
-   def LL(self,x):
-      return 1/(1+10**(-x/400))
-   
-   def LLR(self,W,D,L,elo0,elo1):
-# avoid division by zero
-      """
-      This function computes the log likelihood ratio of H0:elo_diff=elo0 versus
-      H1:elo_diff=elo1 under the logistic elo model
-      
-      expected_score=1/(1+10**(-elo_diff/400)).
-      
-      W/D/L are respectively the Win/Draw/Loss count. It is assumed that the outcomes of
-      the games follow a trinomial distribution with probabilities (w,d,l). Technically
-      this is not quite an SPRT but a so-called GSPRT as the full set of parameters (w,d,l)
-      cannot be derived from elo_diff, only w+(1/2)d. For a description and properties of
-      the GSPRT (which are very similar to those of the SPRT) see
-      
-      http://stat.columbia.edu/~jcliu/paper/GSPRT_SQA3.pdf
-      
-      This function uses the convenient approximation for log likelihood
-      ratios derived here:
-      
-      http://hardy.uhasselt.be/Toga/GSPRT_approximation.pdf
-      
-      The previous link also discusses how to adapt the code to the 5-nomial model
-      discussed above.
-      """
-      if W==0 or D==0 or  L==0:
-         return 0.0
-      N=W+D+L
-      w,d,l=W/N,D/N,L/N
-      s=w+d/2
-      m2=w+d/4
-      var=m2-s**2
-      var_s=var/N
-      s0=self.LL(elo0)
-      s1=self.LL(elo1)
-      return (s1-s0)*(2*s-s0-s1)/var_s/2.0
- 
-   def SPRT(self,W,D,L,elo0,elo1,alpha,beta,games):
-      """
-      This function sequentially tests the hypothesis H0:elo_diff=elo0 versus
-      the hypothesis H1:elo_diff=elo1 for elo0<elo1. It should be called after
-      each game until it returns either 'H0' or 'H1' in which case the test stops
-      and the returned hypothesis is accepted.
-
-      alpha is the probability that H1 is accepted while H0 is true
-      (a false positive) and beta is the probability that H0 is accepted
-      while H1 is true (a false negative). W/D/L are the current win/draw/loss
-      counts, as before.
-      """
-      LLR_=self.LLR(W,D,L,elo0,elo1)
-      LA=math.log(beta/(1-alpha))
-      LB=math.log((1-beta)/alpha)
-      print("LLR=" + "{0:.2f}".format(round(LLR_,2)) + " [" + \
-          "{0:.2f}".format(round(LA,2)) + "," + \
-          "{0:.2f}".format(round(LB,2)) + "] (" + str(games) + " games)")
-      if LLR_>LB:
-          return 'H1'
-      elif LLR_<LA:
-          return 'H0'
-      else:
-          return ''
 
    def get_status(self,host,scores):
       global SSH_CMD, REMOTE_EXEC_SCRIPT
@@ -101,7 +40,7 @@ class Monitor:
          cmd = ""
       else:
          cmd = SSH_CMD + ' -f ' + host + ' '
-         
+
       proc = Popen(cmd + REMOTE_EXEC_SCRIPT, stdout=PIPE, shell=True)
       status_stdout = proc.communicate()[0]
       for line in status_stdout.splitlines():
@@ -110,41 +49,56 @@ class Monitor:
          for chunk in line.decode().split(" "):
             scores[i] = scores[i] + int(chunk)
             i = i + 1
-      proc.wait()    
+      proc.wait()
 
-   def run(self):
-      global scores, STOP_SCRIPT, limit
+   def run(self,machines,limit):
+      global scores, STOP_SCRIPT, ELO_MODEL, POLL_INTERVAL
+      alpha = 0.05
+      beta = 0.05
       result = ""
       while(limit > 0 or len(result)==0):
-         games = 0   
-         time.sleep(180)
+         games = 0
+         time.sleep(POLL_INTERVAL)
          for i in range(0,3):
             scores[i] = 0
-         for host in hosts:
-#            print("querying " + host)
-            self.get_status(host,scores)
+         for host in machines:
+            self.get_status(host['hostname'],scores)
          for i in range(0,3):
             games = games + scores[i]
-#         print("games= " + str(games))   
          if (games == 0):
             continue
          # Note: scores are W L D but from the perspective of the
          # base version. So L W D from the perspective of the version
          # being tested
-         W = scores[1]
-         L = scores[0]
-         D = scores[2]
-         result = self.SPRT(W,D,L,-1.5,4.5,0.05,0.05,games)
-         print("result = %s" % result)
-         if (limit > 0 and games >= limit):
-            break
+         R = {}
+         R['wins'] = scores[1]
+         R['losses'] = scores[0]
+         R['draws'] = scores[2]
+         result = stat_util.SPRT_elo(R,alpha,beta,0.05,0,5.0,ELO_MODEL)
+         LLR = result['LLR']
+         LA=math.log(beta/(1-alpha))
+         LB=math.log((1-beta)/alpha)
+         print("LLR=" + "{0:.2f}".format(round(LLR,2)) + " [" + \
+               "{0:.2f}".format(round(LA,2)) + "," + \
+               "{0:.2f}".format(round(LB,2)) + "] (" + str(games) + " games)")
+         haveResult = True
+         if LLR>LB:
+             result = 'H1'
+         elif LLR<LA:
+             result = 'H0'
+         else:
+             haveResult = False
+         if haveResult:
+             print("result=" + result)
+         if (haveResult or (limit > 0 and games >= limit)):
+             break
       subprocess.call(STOP_SCRIPT,shell=True)
 
 def main(argv = None):
     if argv is None:
         argv = sys.argv[1:]
 
-    global limit
+    limit = 0
     arg = 0
     while ((arg < len(argv)) and (argv[arg][0:1] == '-')):
         if (argv[arg][1] == 'n'):
@@ -159,13 +113,28 @@ def main(argv = None):
             else:
                print("expected number after -n")
                return
-        else:   
+        else:
             print("Unrecognized switch: " + argv[arg], file=sys.stderr)
             return
 
+    try:
+        with open('machines.json', 'r') as machineFile:
+            data=machineFile.read()
+    except:
+        print("machine file not found or could not be opened",file=sys.stderr)
+        exit(1)
+
+    machines = []
+
+    try:
+        machines = json.loads(data)
+    except:
+        print("failed to parse machine file",file=sys.stderr)
+        exit(1)
+
     # start thread
     mon = Monitor()
-    t = threading.Thread(target=mon.run)
+    t = threading.Thread(target=mon.run,args=(machines,limit,))
     t.start()
 
 if __name__ == "__main__":
