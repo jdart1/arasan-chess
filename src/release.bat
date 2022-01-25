23206dd71971551126f8ea314840127399af47b7
Jon Dart <chess1@jondart.name>
2022-01-24 19:32:37 -0800

    update release.bat to include new NNUE file

--- src/release.bat
+++ src/release.bat
@@ -30,5 +30,5 @@ pushd ..
 7z u -tzip -orelease %1 gui\help\*.*
 7z u -tzip -orelease %1 gui\help\html\*.*
 7z u -tzip -orelease %1 gui\install\arasan.iss gui\install\readme.txt
-7z u -tzip -orelease %1 network\arasan-d9-10-20211029.nnue
+7z u -tzip -orelease %1 network\arasan-d9-10-20220124.nnue
 
