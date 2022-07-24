22abf7b0d552916a27e03153667116ee1f434d6d
Jon Dart <chess1@jondart.name>
2022-07-24 13:19:50 -0700

    update default network in Makefile and release.bat

--- src/release.bat
+++ src/release.bat
@@ -30,5 +30,5 @@ pushd ..
 7z u -tzip -orelease %1 gui\help\*.*
 7z u -tzip -orelease %1 gui\help\html\*.*
 7z u -tzip -orelease %1 gui\install\arasan.iss gui\install\readme.txt
-7z u -tzip -orelease %1 network\arasan-d10-20220226.nnue
+7z u -tzip -orelease %1 network\arasan-d10-20220723.nnue
 
