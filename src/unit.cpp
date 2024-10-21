755e8662f7b2a00c393c12b169bc9f1d3d7f97a4
Jon Dart <chess1@jondart.name>
2024-10-21 11:19:26 -0700

    add some sanity-check cases for evaluation

--- src/unit.cpp
+++ src/unit.cpp
@@ -21,6 +21,7 @@
 #include <algorithm>
 #include <cctype>
 #include <cstdio>
+#include <iomanip>
 #include <iostream>
 #include <list>
 #include <regex>
@@ -458,71 +459,55 @@ static int testPGN() {
 }
 
 static int testEval() {
-    // verify eval results are symmetrical (White/Black)
-    const int CASES = 43;
-    static const std::string fens[CASES] = {
-        "8/4K3/8/1NR5/8/4k1r1/8/8 w - -",
-        "8/4K3/8/1N6/6p1/4k2p/8/8 w - -",
-        "8/4K3/8/1r6/6B1/4k2N/8/8 w - -",
-        "3b4/1n3n2/1pk3Np/p7/P4P1p/1P6/5BK1/3R4 b - -",
-        "8/3r1ppk/8/P6P/3n4/2K5/R2B4/8 b - -",
-        "1rb1r1k1/2q2pb1/pp1p4/2n1pPPQ/Pn1BP3/1NN4R/1PP4P/R5K1 b - -",
-        "6k1/1b4p1/5p1p/pq3P2/1p1BP3/1P2QR1P/P1r3PK/8 w - -",
-        "8/5pk1/7p/3p1R2/p1p3P1/2P2K1P/1P1r4/8 w - -",
-        "6k1/p3pp2/6p1/7P/R7/b1q2P2/B1P1K2P/7R b - -",
-        "r7/1b4k1/pp1np1p1/3pq1NN/7P/4P3/PP4P1/1Q3RK1 b - -",
-        "4b3/2p4p/pp1bk3/2p3p1/2P5/PPB2PP1/7P/3K1N2 w - -",
-        "r1bqr1k1/ppp2ppp/3p4/4n3/2PN4/P1Q1P3/1PB2PPP/R4RK1 b - -",
-        "r4rk1/1ppqbppp/p2p1n2/8/1n1PP3/1Q3N2/PP1N1PPP/R1B1R1K1 b - -",
-        "r6k/1p4bp/1p1n1pp1/1B6/8/P4NP1/1P3P1P/2R3K1 w - -",
-        "r1b2r1k/pp3n1p/2p1p3/3Pnppq/3PP3/1P1N1PP1/P5BP/R1Q2RK1 w - -",
-        "2kr3r/1bpnqp2/1p2p3/p2p3p/P1PPBPp1/2P1P1P1/2QN2P1/1R2K2R w K -",
-        "8/1R6/3k4/2p5/2p1B3/5K2/8/8 w - -",
-        "1BR2rk1/pP1nbpp1/B2P2p1/8/8/8/1P4P1/3n2K1 b - -",
-        "r1b1k2r/1p1n1pp1/p6p/2p5/4Nb2/5NP1/PPP2P1P/1K1R1B1R b kq -",
-        "r1b2rk1/1p1n1pp1/p6p/2p5/4Nb2/3R1NP1/PPP2P1P/1K1R1B2 b - -",
-        "1kr5/1p1b2R1/p3p2Q/2bp3P/8/P1PB1P2/1P1K1P2/R6q b - -",
-        "rb3rk1/1p1RRpp1/p6p/r1p5/4Nb2/5NP1/PPP2P1P/1K3B2 b - -",
-        "5rk1/1pqn2pp/4pn2/p7/2P5/4PP2/1B2BP1P/3Q1RK1 w - -",
-        "3k1q2/p3p1p1/1p1nQ3/3P4/P2P4/B2P4/6KP/8 b - -",
-        "6k1/4R1P1/5P2/5K1p/7r/8/8/8 w - -",
-        "1n1q1rk1/4ppbp/3p1np1/1PpP4/4P3/2N2N2/3B1PPP/Q3K2R b K -",
-        "3q1rk1/4ppbp/1n1p1np1/1PpP4/2N1P3/5N2/3B1PPP/Q3K2R b K -",
-        "3q1rk1/4ppbp/1n1p1np1/1P1P4/4P3/2p1BN2/2N2PPP/Q3K2R b K -",
-        "N5r1/pQ6/3b1nkp/2q5/2Pp1p2/4nP2/PP1B2PP/1RR3K1 b - -",
-        "8/2kn2q1/B1p2pP1/P1P2p1p/3P2bP/3P1B2/1K1P1Q2/8 b - -",
-        "5nk1/3b1r2/2p1p3/1pPpP1qp/1P1Q4/6P1/4BN1P/R5K1 w - - 0 1",
-        "r5rk/1R3Q2/2p5/p4K2/5N2/5q2/P7/1R6 b - - 0 43",
-        "2r2bk1/7p/7p/2qPpPP1/pp1p4/P2Q4/1PP5/1K1R3R w - - 0 31",
-        "1r6/2Qnk2p/3p4/5p2/2P5/1P2b1P1/P5KP/8 w - - 0 30",
-        "8/3B2k1/4K1p1/8/8/4B2P/1b6/8 b - - 0 59",
-        "2r5/8/1qr3pk/3p1p1p/R2PpP1P/2Pb2P1/3Q3K/2RB4 b - - 0 52",
-        "5r2/3rq2k/p2Np1p1/1b1pP2p/3Q1P1P/1P4P1/2R3K1/2R5 b - - 0 42",
-        "8/8/8/1K6/3N4/k5p1/6N1/8 b - - 0 92",
-        "1r5k/3nbp2/1r2b1pP/3np3/2B1R3/1NN4q/1PPB2R1/1K3Q2 b - - 0 31",
-        "8/6pk/6p1/4r3/1Qp5/4qPpP/6P1/6RK b - - 0 44",
-        "2k5/1p1b1p2/5n2/p1p1pP2/2PnPq2/3P2r1/PP3QB1/3R1RK1 b - - 0 36",
-        "1r5k/3nbp2/1r2b1pP/3np3/2B1R3/1NN4q/1PPB2R1/1K3Q2 b - - 0 1",
-        "4q3/4Pp2/5P2/1p1N2k1/p5Pn/7P/2B5/5QK1 b - - 0 48"
+
+    struct Case {
+        std::string fen;
+        double minEval, maxEval;
+        Case(const std::string &f, double minE, double maxE) :
+            fen(f),minEval(minE),maxEval(maxE) {
+        }
     };
 
-    int errs = 0;
-    for (int i = 0; i < CASES; i++) {
+    static const std::array<Case,5> cases = {
+        Case("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -",
+             -0.5,0.5),
+        Case("5rk1/1ppb4/p1pb4/6q1/3PRp1r/2P4P/PP1BQ1P1/5RKN b - -",
+             -1.0,1.0),
+        Case("3Q4/P4k2/7p/8/1rP1pn1p/1PKn3B/6P1/8 b - -", // advanced passer
+             -8.0,-2.0),
+        Case("8/6pk/5pb1/7p/Q6P/2r1N3/5PP1/6K1 w - -",4.0,10.0),
+        Case("8/8/4bk2/8/8/4K3/4R3/8 w - -",-0.5,0.5) // even endgame
+    };
+
+    globals::delayedInit();
+    int i = 0, errs = 0;
+    for (const Case &c : cases) {
         Board board;
-        if (!BoardIO::readFEN(board, fens[i].c_str())) {
-            std::cerr << "testEval case " << i << " error in FEN: " << fens[i] << std::endl;
+        if (!BoardIO::readFEN(board, c.fen.c_str())) {
+            std::cerr << "testEval case " << i << " error in FEN: " << c.fen << std::endl;
             ++errs;
             continue;
         }
         Scoring *s = new Scoring();
-        int eval1 = s->evalu8(board);
-        board.flip();
-        int eval2 = s->evalu8(board);
-        if (eval1 != eval2) {
+        score_t eval1 = s->evalu8(board);
+        auto pawnEval = static_cast<double>(eval1)/Params::PAWN_VALUE;
+        if (pawnEval < c.minEval || pawnEval > c.maxEval) {
+            std::cout << "testEval case " << i << " eval exceeds bounds: ";
+            Scoring::printScore(eval1,std::cout);
+            std::ios_base::fmtflags original_flags = std::cout.flags();
+            std::cout << ", expected [" << std::setprecision(2) <<
+                c.minEval << "," << c.maxEval << ']' << std::endl;
+            std::cout.flags(original_flags);
             ++errs;
-            std::cerr << "testEval case " << i << " eval mismatch" << std::endl;
+        }
+        board.flip();
+        score_t eval2 = s->evalu8(board);
+        if (eval1 != eval2) {
+            std::cout << "testEval case " << i << " eval not symmetrical" << std::endl;
+            ++errs;
         }
         delete s;
+        ++i;
     }
     return errs;
 }
