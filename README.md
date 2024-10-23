bad90fbbb502142cf474417bf1c4fe3816349b75
Jon Dart <chess1@jondart.name>
2024-10-23 09:20:54 -0700

    Revert "Remove arasan.rc support from the code."
    This reverts commit f4026a9e01b9869e3f444eb114fd039d6bae4162.

--- README.md
+++ README.md
@@ -72,6 +72,23 @@ while under Linux or MacOS, they should be separated by ":".
 If the word "bench" is specified on the command line, then Arasan will
 run the bench command (for performance reporting) and then exit.
 
+## Option file (arasan.rc)
+
+Many aspects of Arasan's behavior can be modified by changing the
+arasan.rc file. This file is installed in the program directory.
+If present, Arasan will also read a file that in the user's home
+directory:
+
+- on Windows, from arasan.rc
+- on MacOS or Linux, from .arasan.rc
+
+The contents of these files will overwrite the one that is in the
+program directory.
+
+You don't have to use this file to configure Arasan. Most of the options
+in it can also be set from a GUI that understands the UCI or CECP protocol, although there
+are some settings that are only in the arasan.rc file presently.
+
 ## UCI Options
 
 When used as a UCI engine, Arasan supports the following options:
--- src/options.cpp
+++ src/options.cpp
@@ -28,7 +28,7 @@ Options::SearchOptions::SearchOptions()
       syzygy_probe_depth(4),
 #endif
       strength(100), multipv(1), ncpus(1),
-      pureNNUE(false), nnueFile(MAKE_STR(NETWORK)),
+      pureNNUE(false), nnueFile(""),
 #ifdef NUMA
       set_processor_affinity(false),
 #endif
@@ -38,6 +38,26 @@ Options::SearchOptions::SearchOptions()
 
 Options::BookOptions::BookOptions() : variety(50), book_enabled(true), book_path("") {
     globals::openingBook.setVariety(50);
+}
+
+template <class T>
+int Options::setOption(const std::string &name, const std::string &valueString, T &value) {
+    if (!Options::setOption<T>(valueString, value)) {
+        std::cerr << "warning: invalid value for option " << name << std::endl;
+        return 0;
+    }
+    return 1;
+}
+
+static void set_strength_option(const std::string &name, int &value,
+                                const std::string &valueString) {
+    int tmp = value;
+    if (sscanf(valueString.c_str(), "%d", &tmp) != 1 || tmp < 0 || tmp > 100) {
+        std::cerr << "warning: invalid value for option " << name << " (expected integer 0..100)"
+                  << std::endl;
+        return;
+    }
+    value = tmp;
 }
 
 void Options::setMemoryOption(size_t &value, const std::string &valueString) {
@@ -62,5 +82,87 @@ void Options::setMemoryOption(size_t &value, const std::string &valueString) {
         value = (size_t)val * mult;
 }
 
+void Options::set_option(const std::string &name, const std::string &value) {
+    if (name == "store_games") {
+        setOption<bool>(name, value, store_games);
+    } else if (name == "game_pathname") {
+        game_pathname = value;
+    } else if (name == "book.book_enabled") {
+        setOption<bool>(name, value, book.book_enabled);
+    } else if (name == "book.variety") {
+        setOption<unsigned>(name, value, book.variety);
+        globals::openingBook.setVariety(std::min<unsigned>(100,std::max<unsigned>(0, book.variety)));
+    } else if (name == "learning.position_learning") {
+        setOption<bool>(name, value, learning.position_learning);
+    } else if (name == "learning.position_learning.threshold") {
+        int tmp;
+        if (setOption<int>(name, value, tmp)) {
+            learning.position_learning_threshold = (64 * tmp) / 100;
+        }
+    } else if (name == "learning.position_learning.minDepth") {
+        setOption<int>(name, value, learning.position_learning_minDepth);
+    } else if (name == "search.can_resign") {
+        setOption<bool>(name, value, search.can_resign);
+    } else if (name == "search.resign_threshold") {
+        setOption<int>(name, value, search.resign_threshold);
+    } else if (name == "search.hash_table_size") {
+        setMemoryOption(search.hash_table_size, value);
+    }
+#ifdef SYZYGY_TBS
+    else if (name == "search.use_tablebases") {
+        setOption<bool>(name, value, search.use_tablebases);
+    } else if (name == "search.syzygy_path") {
+        search.syzygy_path = value;
+    } else if (name == "search.syzygy_50_move_rule") {
+        setOption<bool>(name, value, search.syzygy_50_move_rule);
+    } else if (name == "search.syzygy_probe_depth") {
+        setOption<int>(name, value, search.syzygy_probe_depth);
+    }
+#endif
+    else if (name == "search.strength") {
+        set_strength_option(name, search.strength, value);
+    } else if (name == "search.ncpus") {
+        setOption<int>(name, value, search.ncpus);
+    } else if (name == "search.nnueFile") {
+        search.nnueFile = value;
+    }
+#ifdef NUMA
+    else if (name == "search.set_processor_affinity") {
+        setOption<bool>(name, value, search.set_processor_affinity);
+    }
+#endif
+    else if (name == "search.move_overhead") {
+        setOption<int>(name, value, search.move_overhead);
+    } else if (name == "search.minimum_search_time") {
+        setOption<int>(name, value, search.minimum_search_time);
+    } else
+        std::cerr << "warning: unrecognized option name: " << name << std::endl;
+}
 
-
+int Options::init(const std::string &fileName) {
+    std::ifstream infile(fileName.c_str());
+    if (!infile.good()) {
+        return -1;
+    }
+    const auto pattern = std::regex("^([^\\s]+)=([^\\s]+([^\\s]+\\w+)*?)\\s*$");
+    std::smatch match;
+    std::string name, value, line;
+    unsigned count = 0;
+    while (infile.good() && !infile.eof()) {
+        getline(infile, line);
+        ++count;
+        if (!line.empty() && line[0] == '#')
+            continue;
+        if (line.empty())
+            continue;
+        if (std::regex_match(line, match, pattern)) {
+            auto it = match.begin() + 1;
+            name = *it++;
+            value = *it;
+            set_option(name, value);
+        } else {
+            std::cerr << "warning: could not parse .rc line " << count << std::endl;
+        }
+    }
+    return 0;
+}
