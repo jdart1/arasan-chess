f3cc69248635f7805ac7bc5c4124c41a6190ecf3
Jon Dart <chess1@jondart.name>
2024-12-11 16:41:05 -0800

    add GUI help files

---
+++ gui/help/csAlias.h
@@ -0,0 +1,3 @@
+; Alias File written automatically by HHPMod Version 3.2.2
+; August 31, 2013, 03:59 PM
+; Automatic aliases for untitled topic files whose text may be extracted to cshelp.txt ;;;;;;;;;;
