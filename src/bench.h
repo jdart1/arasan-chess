e0f21c6766424034cb4610b108c49c2f83320425
Jon Dart <chess1@jondart.name>
2025-07-07 18:17:14 -0700

    increase default hash size for bench command

--- src/bench.h
+++ src/bench.h
@@ -1,5 +1,5 @@
 // Support for the "bench" command
-// Copyright 2020 by Jon Dart. All Rights Reserved.
+// Copyright 2020, 2025 by Jon Dart. All Rights Reserved.
 //
 #ifndef _BENCH_H
 #define _BENCH_H
@@ -22,7 +22,7 @@ public:
         Results() : nodes(0ULL), time(0ULL) {};
     };
 
-    Results bench(int hashSize=1000, int depth=13, int cores=1, bool verbose=false);
+    Results bench(int hashSize=32768, int depth=13, int cores=1, bool verbose=false);
 
     friend std::ostream & operator << (std::ostream &o, const Results &results);
 
